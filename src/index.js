c8fc50a8dac9617f4605822fce3395b2ba84ef29
ArtyomMir <129334598+ArtyomMir@users.noreply.github.com>
2025-11-27 02:06:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,52 +1,86 @@
-import {SpaceX} from "./api/spacex";
+import { SpaceX } from "./api/spacex.js";
 import * as d3 from "d3";
-import * as Geo from './geo.json'
+import Geo from "./geo.json" assert { type: "json" };
 
-document.addEventListener("DOMContentLoaded", setup)
+document.addEventListener("DOMContentLoaded", setup);
 
-function setup(){
-    const spaceX = new SpaceX();
-    spaceX.launches().then(data=>{
-        const listContainer = document.getElementById("listContainer")
-        renderLaunches(data, listContainer);
-        drawMap();
-    })
-}
-function renderLaunches(launches, container){
-    const list = document.createElement("ul");
-    launches.forEach(launch=>{
-        const item = document.createElement("li");
-        item.innerHTML = launch.name;
-        list.appendChild(item);
-    })
-    container.replaceChildren(list);
+async function setup() {
+  const spaceX = new SpaceX();
+
+  // Загружаем данные запусков
+  const launches = await spaceX.launches();
+  const listContainer = document.getElementById("listContainer");
+  renderLaunches(launches, listContainer);
+
+  // Рисуем карту и launchpads
+  drawMap(spaceX, Geo, launches);
 }
 
-function drawMap(){
-    const width = 640;
-    const height = 480;
-    const margin = {top: 20, right: 10, bottom: 40, left: 100};
-    const svg = d3.select('#map').append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
-    const projection = d3.geoMercator()
-        .scale(70)
-        .center([0,20])
-        .translate([width / 2 - margin.left, height / 2]);
-    svg.append("g")
-        .selectAll("path")
-        .data(Geo.features)
-        .enter()
-        .append("path")
-        .attr("class", "topo")
-        .attr("d", d3.geoPath()
-            .projection(projection)
-        )
-        .attr("fill", function (d) {
-            return colorScale(0);
-        })
-        .style("opacity", .7)
+// --- Отображение списка запусков ---
+function renderLaunches(launches, container) {
+  const list = document.createElement("ul");
+  list.style.listStyle = "none";
+  list.style.padding = 0;
+
+  launches.forEach(launch => {
+    const item = document.createElement("li");
+    item.innerHTML = `${launch.name} (${new Date(launch.date_utc).getFullYear()})`;
+    item.style.cursor = "pointer";
+    list.appendChild(item);
+
+    // Событие hover для подсветки launchpad
+    item.addEventListener("mouseenter", () => {
+      d3.selectAll(".launchpad-circle")
+        .attr("fill", d => (d.id === launch.launchpad ? "orange" : "red"))
+        .attr("r", d => (d.id === launch.launchpad ? 8 : 5));
+    });
+    item.addEventListener("mouseleave", () => {
+      d3.selectAll(".launchpad-circle")
+        .attr("fill", "red")
+        .attr("r", 5);
+    });
+  });
+
+  container.replaceChildren(list);
+}
+
+// --- Рисуем карту и launchpads ---
+function drawMap(spaceX, Geo, launches) {
+  const width = 800;
+  const height = 500;
+
+  const svg = d3.select("#map").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  const projection = d3.geoMercator()
+    .fitSize([width, height], Geo); // Автоцентрирование и масштаб
+
+  const path = d3.geoPath().projection(projection);
+
+  // Рисуем страны
+  svg.selectAll("path")
+    .data(Geo.features)
+    .enter()
+    .append("path")
+    .attr("d", path)
+    .attr("fill", "lightgray")
+    .attr("stroke", "black")
+    .attr("stroke-width", 0.5);
+
+  // Загружаем launchpads
+  spaceX.launchpads().then(pads => {
+    svg.selectAll("circle")
+      .data(pads)
+      .enter()
+      .append("circle")
+      .attr("class", "launchpad-circle")
+      .attr("cx", d => projection([d.longitude, d.latitude])[0])
+      .attr("cy", d => projection([d.longitude, d.latitude])[1])
+      .attr("r", 5)
+      .attr("fill", "red")
+      .attr("stroke", "white")
+      .append("title")
+      .text(d => d.name);
+  });
 }
